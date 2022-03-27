62039a55df11285dfbcdcf81f73ea7abcfb4494e
Mario-ECS <mcontreras.se777@gmail.com>
2022-03-27 02:34:17 -0600

    feature: customizing config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Parrot - Sr. Frontend challenge`,
         short_name: `Parrot challenge`,
         start_url: `/`,
         background_color: `#f04e4a`,
